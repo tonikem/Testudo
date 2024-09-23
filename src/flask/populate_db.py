f13af34cf277f4f743a052af8ec0b34d844fa61c
Toni Kemppainen <tonikem@gmail.com>
2024-09-23 16:18:39 +0300

    Käyttäjät tietokantaan

--- src/flask/populate_db.py
+++ src/flask/populate_db.py
@@ -10,6 +10,7 @@ testudo_data_db = mongo_client["TestudoData"]
 
 # Tietokanta sarakkeet
 notebooks_col = testudo_data_db["notebooks"]
+admin_users_col = testudo_users_db["admin_users"]
 
 
 with open("./templates/data.json") as file:
@@ -18,8 +19,19 @@ with open("./templates/data.json") as file:
 
     # Poistetaan ensin kaikki Notebookit
     notebooks_col.delete_many({})
+
     # Asetetaan sitten data
     notebooks_col.insert_many(notebooks)
 
+
+with open("./templates/users.json") as file:
+    data = json.load(file)
+    users = data["users"]
+
+    # Poistetaan ensin kaikki käyttäjät
+    admin_users_col.delete_many({})
+
+    # Asetetaan sitten data
+    admin_users_col.insert_many(users)
 
 
