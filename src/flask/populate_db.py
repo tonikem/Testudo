eafbb8c882644e38d5ea6bacc10277398afd3e57
Toni Kemppainen <tonikem@gmail.com>
2024-09-23 16:00:46 +0300

    Data tietokantaan

--- src/flask/populate_db.py
+++ src/flask/populate_db.py
@@ -3,16 +3,23 @@ from pymongo import MongoClient
 
 
 mongo_client = MongoClient("localhost", 27017)
+
+# Tietokannat
 testudo_users_db = mongo_client["TestudoUsers"]
 testudo_data_db = mongo_client["TestudoData"]
+
+# Tietokanta sarakkeet
+notebooks_col = testudo_data_db["notebooks"]
 
 
 with open("./templates/data.json") as file:
     data = json.load(file)
     notebooks = data["main"]
 
-    for notebook in notebooks:
-        print(notebook)
+    # Poistetaan ensin kaikki Notebookit
+    notebooks_col.delete_many({})
+    # Asetetaan sitten data
+    notebooks_col.insert_many(notebooks)
 
 
 
