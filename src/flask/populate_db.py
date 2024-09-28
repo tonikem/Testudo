adc10c9956749a29feac270af0fc8fa372595ae6
Toni <tonikem@gmail.com>
2024-09-28 09:14:10 +0300

    Populointi

--- src/flask/populate_db.py
+++ src/flask/populate_db.py
@@ -11,28 +11,28 @@ notebooks_db = server.database("notebooks")
 users_db = server.database("users")
 
 
-#with open("./templates/data.json") as file:
-#    data = json.load(file)
-#    notebooks = data["main"]
-#
-#    # Asetetaan notebook data
-#    notebooks_db.save_bulk(notebooks)
+with open("./templates/data.json") as file:
+    data = json.load(file)
+    notebooks = data["main"]
+
+    # Asetetaan notebook data
+    notebooks_db.save_bulk(notebooks)
 
 
-#with open("./templates/users.json") as file:
-#    data = json.load(file)
-#    users = data["users"]
-#
-#    # Asetetaan user data
-#    for user in users:
-#        password = get_hashed_password(user['password'].strip())
-#        username = user['name'].strip()
-#        user = {
-#            "name": username,
-#            "password": password.decode('utf-8'),
-#            "id": user['id'],
-#            "notebooks": user["notebooks"]
-#        }
-#        users_db.save(user)
+with open("./templates/users.json") as file:
+    data = json.load(file)
+    users = data["users"]
+
+    # Asetetaan user data
+    for user in users:
+        password = get_hashed_password(user['password'].strip())
+        username = user['name'].strip()
+        user = {
+            "name": username,
+            "password": password.decode('utf-8'),
+            "id": user['id'],
+            "notebooks": user["notebooks"]
+        }
+        users_db.save(user)
 
 
