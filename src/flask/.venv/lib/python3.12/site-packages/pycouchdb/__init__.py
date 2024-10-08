20d21e11efa1e79499d29c860a5c5f03f894baff
Toni Kemppainen <tonikem@gmail.com>
2024-10-08 18:10:00 +0300

    Venv ja build.sh

---
+++ src/flask/.venv/lib/python3.12/site-packages/pycouchdb/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Andrey Antukh"
+__license__ = "BSD"
+__version__ = "1.16.0"
+__maintainer__ = "Rinat Sabitov"
+__email__ = "rinat.sabitov@gmail.com"
+__status__ = "Development"
+
+
+from .client import Server  # noqa: F401
