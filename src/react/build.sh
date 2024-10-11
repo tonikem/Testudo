7adb3a156dcaba1a3a65b2d1ef94e7d3467c4f64
Toni Kemppainen <tonikem@gmail.com>
2024-10-11 19:44:12 +0300

    Pieni muutos

--- src/react/build.sh
+++ src/react/build.sh
@@ -1,3 +1,2 @@
-cp -R "dist\assets\index.html" "..\flask\templates\"
-cp -R "dist\assets\static\*.css" "..\flask\static\"
-cp -R "dist\assets\static\*.js" "..\flask\static\"
+cp -R "dist/assets/index.html" "../flask/templates/"
+cp -R "dist/assets/static" "../flask/static/"
